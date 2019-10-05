7475ca345e1597cc00ebd973ace80259c873cd58
jcampos- <jcampos-@c2r15s1.42madrid.com>
2019-10-05 18:11:45 +0200

    bloquelogico añadido a rush

--- ex00/main.c
+++ ex00/main.c
@@ -6,12 +6,15 @@
 /*   By: jcampos- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/05 11:42:02 by jcampos-          #+#    #+#             */
-/*   Updated: 2019/10/05 12:14:46 by jcampos-         ###   ########.fr       */
+/*   Updated: 2019/10/05 16:09:42 by jcampos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include <unistd.h>
+
+void rush(int x, int y);
+void write(char c);
 
 int		main()
 {
