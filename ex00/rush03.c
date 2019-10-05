84c948d2a210461b6961d13ef7aa8c1e00d14be0
jcampos- <jcampos-@c2r15s1.42madrid.com>
2019-10-05 19:45:51 +0200

    Hecho programa principal

--- ex00/rush03.c
+++ ex00/rush03.c
@@ -6,45 +6,72 @@
 /*   By: jcampos- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/05 12:07:16 by jcampos-          #+#    #+#             */
-/*   Updated: 2019/10/05 18:11:15 by jcampos-         ###   ########.fr       */
+/*   Updated: 2019/10/05 19:43:15 by jcampos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void rush(char x, char y)
+void	cond1(int x, int l, int h);
+void	cond2(int x, int l, int h);
+
+void	rush(int x, int y)
 {
-	char a;
-	char b;
-	char c;
 	int l;
 	int h;
 
-	a = 'A';
-	b = 'B';
-	c = 'C';
-	l = 0;
-	h = 0;
+	l = 1;
+	h = 1;
 	
 	while(l <= x && h <= y) 
 	{
-		while(h == 0 || h == y)
+		while(h == 1 || h == y)
 		{
-			if(l == 0 )
-			{
-				//putchar A
-				//l++
-			}
-			else if(l == x)
-			{
-				//putchar C
-				//l=0
-				//h++
-			}
-			else //putchar B
-				//l++
+			cond1(x, l, h);
 		}
-		while (h =! 0 && h =! y)
+		while (h =! 1 && h =! y)
 		{
-			
+			cond2(x, l, h)
 		}
+	}
+
+void 	cond1(int x, int l, int h)
+{
+	if(l == 1 )
+	{
+		ft_putchar('A');				//putchar A
+		l++;							//l++
+	}
+	else if(l == x)	
+	{
+		ft_putchar('C');				//putchar C
+		l = 1;							//l=1
+		h++;							//h++
+	}
+	else 
+	{
+		ft_putchar('B');				//putchar B
+		l++;							//l++
+	}
+}
+
+void 	cond2(int x, int l, int h)
+{
+	if( l == 1)
+	{
+		ft_putchar('B');
+		l++;
+	}
+	else if( l == x )
+	{
+		ft_putchar('B');
+		l = 1;
+		h++;
+	}
+	else
+	{
+		ft_putchar(' ');
+		l++;
+	}
+}
+
